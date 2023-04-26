b389b0c615f00426a7aa8371f0eca982f68b1b64
Carlos Olivares <110266729+c-olivares@users.noreply.github.com>
2023-04-26 13:13:25 -0300

    Update UserController.java

--- src/main/java/com/backend/olivares/controller/UserController.java
+++ src/main/java/com/backend/olivares/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://portfolio-carlos-olivares.web.app/https://portfolio-carlos-olivares.web.app/")
 public class UserController {
 
     @Autowired
